08da5a8b405f1794eb6c0b601236723639b75b93
RyunosukeAkagawa <ryunosuke19910601@gmail.com>
2023-11-01 15:57:14 +0900

    createページ、store作成

--- app/Http/Controllers/PlanetController.php
+++ app/Http/Controllers/PlanetController.php
@@ -14,6 +14,31 @@ class PlanetController extends Controller
         return view('planets.index', ['planets' => $planets]);
     }
 
+    //createページ
+    public function create()
+    {
+        return view('planets.create');
+    }
+
+    //storeページ
+    public function store(Request $request)
+    {
+        //インスタンスの作成
+        $planet = new Planet;
+
+        // 値の用意
+        $planet->name = $request->name;
+        $planet->name_en = $request->name_en;
+        $planet->radius = $request->radius;
+        $planet->weight = $request->weight;
+
+        //インスタンスに値を設定して保存
+        $planet->save();
+
+        //登録したらindexに戻る
+        return redirect(route("planets.index"));
+    }
+
     //showページ
     public function show($id)
     {
